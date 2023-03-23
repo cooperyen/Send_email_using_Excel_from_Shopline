c9c465005a019753064d059964db4956c87347c1
cooperyen <dknick081@gmail.com>
2023-03-23 21:22:43 +0800

    0323

--- autoLogin.py
+++ autoLogin.py
@@ -1,41 +1,25 @@
-import os
+
+from create_chrome import createChrome
+from webdriver_setting import driver, driverURL
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# default setting.
-#
-proxy = '127.0.0.1'
-port = '9527'
-ip = f'{proxy}:{port}'
 
-
-# cmd setting.
-#
-# ** (A). automatically open chrome and login as a user
-def createChrome():
-    chromePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
-    chromeUserLoginTemporaryData = 'C:/selenium/AutomationProfile'
-    cmdCommand = f'"{chromePath}" --remote-debugging-port={port} --user-data-dir="{chromeUserLoginTemporaryData}"'
-    os.popen(cmdCommand)
-
-
+# ** (1). automatically open chrome and login as a user
 createChrome()
 
-# webdriver handler
-#
-# ** control (A) and go to admin page
-chromeOptions = Options()
-chromeOptions.add_experimental_option("debuggerAddress", ip)
-chromeService = Service(r'./chromedriver.exe')
-driver = webdriver.Chrome(service=chromeService, options=chromeOptions)
-URL = 'https://admin.shoplineapp.com/admin/rafagogorafa154/'
-driver.get(URL)
+# ** (2). control (1) and go to admin page
+driver = driver()
+driver.get(driverURL)
 
 a = driver.find_element(
     By.XPATH, '//div[@data-e2e-id="sidebar_customer_management_menu"]')
 
-print(a.text)
-input()
+b = driver.find_element(
+    By.XPATH, '//a[@data-e2e-id="sidebar_customer_management_submenu_users"]')
+
+print(a.click())
+print(b.click())
+
+
+sleep(10)
---
+++ create_chrome.py
@@ -0,0 +1,11 @@
+import os
+from setting import port
+
+# cmd setting.
+
+def createChrome():
+    chromePath = 'C:/Users/coope/AppData/Local/Google/Chrome/Application/chrome.exe'
+    # chromePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+    chromeUserLoginTemporaryData = 'C:/selenium/AutomationProfile'
+    cmdCommand = f'"{chromePath}" --remote-debugging-port={port} --user-data-dir="{chromeUserLoginTemporaryData}"'
+    os.popen(cmdCommand)
