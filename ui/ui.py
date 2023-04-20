d7d5640a51785f6be0cfc11906704cdb95c2c66d
cooperyen <dknick081@gmail.com>
2023-04-20 17:42:00 +0800

    0420

--- ui/ui.py
+++ ui/ui.py
@@ -88,7 +88,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
 
 
 
-class ToplevelWindows(customtkinter.CTkToplevel):
+class App(customtkinter.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.EXECL_HANDLER = EXECL_HANDLER()
@@ -104,7 +104,7 @@ class ToplevelWindows(customtkinter.CTkToplevel):
         self.geometry('%dx%d+%d+%d' % (self.width, self.height, self.x, self.y))
         self.minsize(self.width, self.height)
         self.appTitle = 'ALL FOR ONE.'
-        self.resizable(False, False)
+        # self.resizable(False, False)
 
         # configure grid layout (4x4).
         self.grid_columnconfigure(1, weight=1)
@@ -624,38 +624,38 @@ class ToplevelWindows(customtkinter.CTkToplevel):
 
 
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        # configure window.
-        self.displayWidth = self.winfo_screenwidth()  # Width of the screen
-        self.displayHeight = self.winfo_screenheight()  # Height of the screen
-        self.width = 1400
-        self.height = 800
-        self.x = (self.displayWidth/2) - (self.width/2)
-        self.y = (self.displayHeight/2) - (self.height/2)
-        self.title("All for one")
-        self.geometry('%dx%d+%d+%d' %
-                      (self.width, self.height, self.x, self.y))
-        self.minsize(self.width, self.height)
-        self.appTitle = 'ALL FOR ONE.'
+# class App(customtkinter.CTk):
+#     def __init__(self):
+#         super().__init__()
+#         # configure window.
+#         self.displayWidth = self.winfo_screenwidth()  # Width of the screen
+#         self.displayHeight = self.winfo_screenheight()  # Height of the screen
+#         self.width = 1400
+#         self.height = 800
+#         self.x = (self.displayWidth/2) - (self.width/2)
+#         self.y = (self.displayHeight/2) - (self.height/2)
+#         self.title("All for one")
+#         self.geometry('%dx%d+%d+%d' %
+#                       (self.width, self.height, self.x, self.y))
+#         self.minsize(self.width, self.height)
+#         self.appTitle = 'ALL FOR ONE.'
 
-        self.button = customtkinter.CTkButton(master=self,
-                                              width=120,
-                                              height=32,
-                                              border_width=0,
-                                              corner_radius=8,
-                                              text="CTkButton",
-                                              command=self.button_events)
-        self.button.place(relx=0.5, rely=0.5, anchor='center')
-        self.toplevel_window = None
+#         self.button = customtkinter.CTkButton(master=self,
+#                                               width=120,
+#                                               height=32,
+#                                               border_width=0,
+#                                               corner_radius=8,
+#                                               text="CTkButton",
+#                                               command=self.button_events)
+#         self.button.place(relx=0.5, rely=0.5, anchor='center')
+#         self.toplevel_window = None
 
-    def button_events(self):
-        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            # create window if its None or destroyed
-            self.toplevel_window = ToplevelWindows(self)
-            self.toplevel_window.grab_set() 
-        else:
-            self.toplevel_window.focus()  # if window exists focus it
+#     def button_events(self):
+#         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+#             # create window if its None or destroyed
+#             self.toplevel_window = ToplevelWindows(self)
+#             self.toplevel_window.grab_set() 
+#         else:
+#             self.toplevel_window.focus()  # if window exists focus it
 
 
