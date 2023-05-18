55d0b297a524fd3bc21f5ba72200521fb06f7d21
cooperyen <dknick081@gmail.com>
2023-05-18 18:09:20 +0800

    0518

--- d.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://api.millionverifier.com/api/v3/?api=Ob3F6xDemZzkFV4QVppLjzOvs&email=cooper.rafagodfsdfs@gmdail.com&timeout=10"
-
-response = requests.request("GET", url)
-
-print(response.text)
